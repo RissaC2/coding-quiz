61645318a61881fccd90bd883d9ed49f7724daa3
Clarissa Cole <clarissa_2006@hotmail.co.uk>
2023-02-11 21:51:44 +0000

    added all function to skip to next question

--- 04-code-quiz-lesson/starter/assets/script.js
+++ 04-code-quiz-lesson/starter/assets/script.js
@@ -4,9 +4,10 @@ const firstQuestionTitle = document.querySelector("#questions");
 const firstQuestionChoices = document.querySelector("#choices");
 const secondQuestionTitle = document.querySelector("#questionstwo");
 const secondQuestionChoices = document.querySelector("#choices2");
-// const resultsContainer = document.getElementById('results');
-// const submitButton = document.getElementById('submit');
-// submitButton.addEventListener('click', showResults);
+const thirdQuestionTitle = document.querySelector("#questionsthree");
+const fourthQuestionTitle = document.querySelector("#questionsfour");
+const fifthQuestionTitle = document.querySelector("#questionsfive");
+const endResult= document.querySelector("end-screen")
   
   
   document.querySelector(".start").addEventListener("click", function () {
@@ -20,23 +21,31 @@ const secondQuestionChoices = document.querySelector("#choices2");
   });
 
 
-
   firstQuestionTitle.addEventListener("click", function() {
     firstQuestionTitle.classList.add("hide");
     secondQuestionTitle.classList.remove("hide");
   });
-  
-//   document.querySelector(firstQuestionTitle).addEventListener("click", function () {
-//     document.querySelector().classList.add(secondQuestionTitle);
-//     displayQuestion();
-    
-//   });
-  
-//   document.querySelector(firstQuestionTitle).addEventListener("click", function () {
-//     questions.classList.remove("hide");
-//   });
 
+  secondQuestionTitle.addEventListener("click", function() {
+    secondQuestionTitle.classList.add("hide");
+    thirdQuestionTitle.classList.remove("hide");
+  });
+  thirdQuestionTitle.addEventListener("click", function() {
+    thirdQuestionTitle.classList.add("hide");
+    fourthQuestionTitle.classList.remove("hide");
+  });
+  fourthQuestionTitle.addEventListener("click", function() {
+    fourthQuestionTitle.classList.add("hide");
+    fifthQuestionTitle.classList.remove("hide");
+  });
+  fifthQuestionTitle.addEventListener("click", function() {
+    fifthQuestionTitle.classList.add("hide");
+    endResult.classList.remove("hide");
+  });
   
+// const resultsContainer = document.getElementById('results');
+// const submitButton = document.getElementById('submit');
+// submitButton.addEventListener('click', showResults);
 
 
 //   
