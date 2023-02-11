5a800322bab126d9e969c70382f36f71fad643a9
Clarissa Cole <clarissa_2006@hotmail.co.uk>
2023-02-11 20:45:06 +0000

    corrected the function for skipping to next question

--- 04-code-quiz-lesson/starter/assets/script.js
+++ 04-code-quiz-lesson/starter/assets/script.js
@@ -1,38 +1,64 @@
-var startbutton = document.querySelector(".start");
-var questions = document.querySelector(".hide");
-
-
-
-document.querySelector("#start").addEventListener("click", function () {
-    questions.classList.remove("hide");
+var Correctanswer = document.querySelector("#correct");
+const questionscontainer = document.getElementById('questions');
+const firstQuestionTitle = document.querySelector("#questions");
+const firstQuestionChoices = document.querySelector("#choices");
+const secondQuestionTitle = document.querySelector("#questionstwo");
+const secondQuestionChoices = document.querySelector("#choices2");
+// const resultsContainer = document.getElementById('results');
+// const submitButton = document.getElementById('submit');
+// submitButton.addEventListener('click', showResults);
+  
+  
+  document.querySelector(".start").addEventListener("click", function () {
+    document.querySelector("#start-screen").classList.add("hide");
+    displayQuestion();
+    
   });
   
+  document.querySelector("#start").addEventListener("click", function () {
+    questions.classList.remove("hide");
+  });
 
 
+
+  firstQuestionTitle.addEventListener("click", function() {
+    firstQuestionTitle.classList.add("hide");
+    secondQuestionTitle.classList.remove("hide");
+  });
+  
+//   document.querySelector(firstQuestionTitle).addEventListener("click", function () {
+//     document.querySelector().classList.add(secondQuestionTitle);
+//     displayQuestion();
+    
+//   });
+  
+//   document.querySelector(firstQuestionTitle).addEventListener("click", function () {
+//     questions.classList.remove("hide");
+//   });
+
+  
+
+
+//   
 //list questioms for quiz
-var li1 = document.createElement("li");
-var li2 = document.createElement("li");
-var li3 = document.createElement("li");
-var li4 = document.createElement("li");
+// var li1 = document.createElement("li");
+// var li2 = document.createElement("li");
+// var li3 = document.createElement("li");
+// var li4 = document.createElement("li");
 
+// var timel = document.querySelector(".timer");
 
-var timel = document.querySelector(".timer")
+// var secondsleft = 50;
 
-var secondsleft = 50
+// function setTime() {
+//   var timerInterval = setInterval(function () {
+//     secondsLeft--;
+//     timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
 
+//     if (secondsLeft === 0) {
+//       clearInterval(timerInterval);
 
-function setTime() {
- 
-    var timerInterval = setInterval(function() {
-      secondsLeft--;
-      timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
-  
-      if(secondsLeft === 0) {
-   
-        clearInterval(timerInterval);
-      
-        sendMessage();
-      }
-  
-    }, 1000000);
-  }
+//       sendMessage();
+//     }
+//   }, 1000000);
+// }
