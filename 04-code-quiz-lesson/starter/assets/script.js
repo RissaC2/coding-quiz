7d59704086838f822de18c7106900119723489d4
Clarissa Cole <clarissa_2006@hotmail.co.uk>
2023-02-12 18:37:29 +0000

    added code to track highscore and deduct time if wrong

--- 04-code-quiz-lesson/starter/assets/script.js
+++ 04-code-quiz-lesson/starter/assets/script.js
@@ -1,5 +1,5 @@
-var Correctanswer = document.querySelector("#correct");
-const questionscontainer = document.getElementById('questions');
+var correctAnswer = document.querySelector("#correct");
+const questionscontainer = document.getElementById("questions");
 const firstQuestionTitle = document.querySelector("#questions");
 const firstQuestionChoices = document.querySelector("#choices");
 const secondQuestionTitle = document.querySelector("#questionstwo");
@@ -7,58 +7,61 @@ const secondQuestionChoices = document.querySelector("#choices2");
 const thirdQuestionTitle = document.querySelector("#questionsthree");
 const fourthQuestionTitle = document.querySelector("#questionsfour");
 const fifthQuestionTitle = document.querySelector("#questionsfive");
-const endResult= document.querySelector("end-screen")
-var timer = document.getElementById("time"); 
+const endResult = document.querySelector("end-screen");
+var timer = document.getElementById("time");
 var timeLeft = 75;
+var highscores = document.getElementById("highscores");
+var score = 0;
 
+document.querySelector(".start").addEventListener("click", function () {
+  document.querySelector("#start-screen").classList.add("hide");
+  displayQuestion();
+});
 
-  
-  
-  document.querySelector(".start").addEventListener("click", function () {
-    document.querySelector("#start-screen").classList.add("hide");
-    displayQuestion();
-    
-  });
-  
-  document.querySelector("#start").addEventListener("click", function () {
-    questions.classList.remove("hide");
-  });
+document.querySelector("#start").addEventListener("click", function () {
+  questions.classList.remove("hide");
+});
 
+firstQuestionTitle.addEventListener("click", function () {
+  firstQuestionTitle.classList.add("hide");
+  secondQuestionTitle.classList.remove("hide");
+});
 
-  firstQuestionTitle.addEventListener("click", function() {
-    firstQuestionTitle.classList.add("hide");
-    secondQuestionTitle.classList.remove("hide");
-  });
+secondQuestionTitle.addEventListener("click", function () {
+  secondQuestionTitle.classList.add("hide");
+  thirdQuestionTitle.classList.remove("hide");
+});
+thirdQuestionTitle.addEventListener("click", function () {
+  thirdQuestionTitle.classList.add("hide");
+  fourthQuestionTitle.classList.remove("hide");
+});
+fourthQuestionTitle.addEventListener("click", function () {
+  fourthQuestionTitle.classList.add("hide");
+  fifthQuestionTitle.classList.remove("hide");
+});
+fifthQuestionTitle.addEventListener("click", function () {
+  fifthQuestionTitle.classList.add("hide");
+  endResult.classList.remove("hide");
+});
 
-  secondQuestionTitle.addEventListener("click", function() {
-    secondQuestionTitle.classList.add("hide");
-    thirdQuestionTitle.classList.remove("hide");
-  });
-  thirdQuestionTitle.addEventListener("click", function() {
-    thirdQuestionTitle.classList.add("hide");
-    fourthQuestionTitle.classList.remove("hide");
-  });
-  fourthQuestionTitle.addEventListener("click", function() {
-    fourthQuestionTitle.classList.add("hide");
-    fifthQuestionTitle.classList.remove("hide");
-  });
-  fifthQuestionTitle.addEventListener("click", function() {
-    fifthQuestionTitle.classList.add("hide");
-    endResult.classList.remove("hide");
-  });
+function startTimer() {
+  timeInterval = setInterval(function () {
+    timeLeft--;
+    timer.textContent = "Time: " + timeLeft;
 
+    if (timeLeft <=0) {
+      
+    }
+  }, 1000);
+}
+document.querySelector("#start").addEventListener("click", startTimer);
 
- 
+function checkAnswer(answer) {
 
-  function startTimer() {
-    timeInterval = setInterval(function () {
-        timeLeft--;
-        timer.textContent = "Time: " + timeLeft;
- 
-        if (timeLeft < 0) {
-            displayScore(score);
-            clearInterval(timeInterval);
-        }
-    }, 1000);
- }
- document.querySelector("#start").addEventListener("click", startTimer);
+  if (answer === correctAnswer) {
+    score += 10;
+  } else {
+    timeLeft -= 10;
+  }
+}
+document.querySelector("highscores").addEventListener("click",checkAnswer);
